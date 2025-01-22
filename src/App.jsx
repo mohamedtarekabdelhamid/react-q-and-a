c537825ae36a6ebecb0c86c1510d85e2a7556a62
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-01-22 20:05:52 +0200

    start the q-a project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>
+                    Example heading
+                    <Badge bg="secondary" as={Button}>
+                        New
+                    </Badge>
+                </h1>
+            </div>
+            ;
+        </>
+    );
+}
+
+export default App;
