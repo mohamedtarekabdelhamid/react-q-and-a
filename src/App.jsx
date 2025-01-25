a8a8b177f00dd8a52145a5e6ce4e8c514fef76df
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-01-25 13:59:08 +0200

    Import the main component of the project in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,57 @@
-import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormInput from './components/FormInput';
+import QAAccordion from './components/QAAccordion';
 
 function App() {
+    const [questions, setQuestions] = useState([]);
+
+    const deleteQuestionsHandler = (e) => {
+        e.preventDefault();
+        setQuestions([]);
+    };
     return (
         <>
-            <div>
-                <h1>
-                    Example heading
-                    <Badge bg="secondary" as={Button}>
-                        New
-                    </Badge>
-                </h1>
+            <div className="font color-body">
+                <Container>
+                    <Row>
+                        <Col
+                            md={3}
+                            className="mt-5 text-center align-items-center"
+                        >
+                            <h3>قائمة الأسئلة السابقة</h3>
+                        </Col>
+                        <Col md={9} className="mt-5 text-center">
+                            <FormInput setQuestions={setQuestions} />
+                            {!questions.length ? (
+                                <h6 className="mt-5">لا يوجد اسئلة حاليا</h6>
+                            ) : (
+                                questions.map((question, index) => (
+                                    <QAAccordion
+                                        key={index}
+                                        data={question}
+                                        setQuestions={setQuestions}
+                                    />
+                                ))
+                            )}
+                            {questions.length ? (
+                                <button
+                                    className="btn-color px-3 py-2 mb-3 w-100"
+                                    type="submit"
+                                    onClick={deleteQuestionsHandler}
+                                >
+                                    حذف كل الاسئلة
+                                </button>
+                            ) : (
+                                ''
+                            )}
+                        </Col>
+                    </Row>
+                </Container>
             </div>
-            ;
         </>
     );
 }
